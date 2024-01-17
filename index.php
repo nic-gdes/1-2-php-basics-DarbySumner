13c42fc4cf2c75a8a6562cc954c8e3c634662b36
Darby <dssumner@nic.edu>
2024-01-17 15:51:25 -0800

    Creating variables

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<?php
+
+// One variable for each data type
+    $myCar = "car";
+    $make = "Ford";
+    $model = "Taurus";
+    $year = 2004;
+    
+
+      
+?>
+
+<?php
+class Car {
+    public $make;
+}
+?>
