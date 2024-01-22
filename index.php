7fffbe9150876c0a31a36a1ba0da9433f915e8f6
Darby <dssumner@nic.edu>
2024-01-22 14:16:22 -0800

    Went through steps 1-4 of the variables of the assignment and moving on to 5

--- index.php
+++ index.php
@@ -6,13 +6,37 @@
     $make = "Ford";
     $model = "Taurus";
     $year = 2004;
-    
 
-      
-?>
+// Echo the following strings as a single concatenated sentence
+    echo "I " . "love " . "PHP " . "!"
 
-<?php
-class Car {
-    public $make;
-}
+// Create an array called myArray with 4 pieces of data
+    $my_array = [1,2,3,4];
+
+// Create a class called car and include, make, model and year
+    class Car {
+        public $make;
+        public $model;
+        public $year;
+        
+    }
+
+    function set_make($make) {
+        $this->make = $make; 
+    }
+
+    function set_model($model) {
+        $this->model = $model; 
+    }
+
+    function set_year($year) {
+        $this->year = $year; 
+    };
+
+// Create a variable called myCar that is an object of type
+
+
+
+
+
 ?>
