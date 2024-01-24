dc3695326850e770d523149de80c6fb6bc4cb329
Darby <dssumner@nic.edu>
2024-01-24 14:15:07 -0800

    fixed problems with creating phrase for my car

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
     $make = "Ford";
     $model = "Taurus";
     $year = 2004;
-    $my_car = new Car();
+   
 // Echo the following strings as a single concatenated sentence
     echo "I " . "hate " . "PHP " . "!";
 
 // Create an array called myArray with 4 pieces of data
+    $integer = 30;
     $my_array = [1,2,3,4];
 
 // Create a class called car and include, make, model and year
@@ -18,22 +19,21 @@
         public $model;
         public $year;
         
-    }
-
-    function set_make($make) {
+    function set_properties($make, $model, $year) {
         $this->make = $make; 
-    }
-
-    function set_model($model) {
         $this->model = $model; 
-    }
-
-    function set_year($year) {
         $this->year = $year; 
-    };
+    }
+}
 
 // echo the phrase I have a {{ year }} {{ make }} {{ model }}".
-    echo "I have a " , $my_car->year, " " . $my_car->make . " " . $my_car->model;
+    $my_car = new Car();
+    $my_car->set_properties("2004","Ford","Taurus");
+    echo "I have a " . $my_car->year. " " . $my_car->make . " " . $my_car->model . " ";
+
+//Write a single if statement that checks if the value of your $integer variable is greater than or equal to 10. 
+//Create a true and false condition (if/else).
+
 
 
 ?>
